f7d8394c376c915b3bbf8509a4d7c09933db8f45
stateofstats <depepeterson23@gmail.com>
2017-08-27 13:57:16 -0500

    dual path, df fix

--- win_prob.R
+++ win_prob.R
@@ -27,6 +27,8 @@ gamestoday <- function(season, date) {
 
 
 # import ratings to use for win probability
+
+# Macbook path
 importratings <- function(gamesdate) {
   list.files(setwd("/Users/derekpeterson/Documents/R/Elo/2018"))
   gamesdate <- as.Date(gamesdate)
@@ -35,9 +37,18 @@ importratings <- function(gamesdate) {
   return(ratings)
 }
 
+importratings <- function(gamesdate) {
+  list.files(setwd("~/R/Elo/Elo2/2018/"))
+  gamesdate <- as.Date(gamesdate)
+  filen <- paste("~/R/Elo/Elo2/2018/",gamesdate,"start.csv", sep = "")
+  ratings <- read.csv(file = filen)  
+  return(ratings)
+}
+
 
 # Construct initial objects
 ratings_today <- importratings("2017-10-04")
+ratings_today$X <- NULL
 
 games_today <- gamestoday(2018, "2017-10-04")
 games_today
@@ -50,17 +61,37 @@ games_today$eh <- round(1/(1 + 10^-((games_today$eloh-games_today$elov + home)/4
 games_today$ev <- 100-games_today$eh
 
 
-
-
 games <- data.frame(Date = games_today$Date,
-                    Away = games_today$Visitor,
-                    eV = paste(round(games_today$ev,2),"%", sep = ""),
-                    Home = games_today$Home,
-                    eH = paste(round(games_today$eh,2),"%", sep = ""),
-                    Tweet = paste(games$Date, games$Away, "at", games$Home, ":", games$Away, games$eV, "chance to win /", games$Home, games$eH, "chance to win")
-)
+           Away = games_today$Visitor,
+           eV = paste(round(games_today$ev,2),"%", sep = ""),
+           Home = games_today$Home,
+           eH = paste(round(games_today$eh,2),"%", sep = ""))
+games$Tweet <- paste(games$Date,
+                     games$Away, "at",
+                     games$Home, ":",
+                     games$Away, games$eV, "chance to win /",
+                     games$Home, games$eH, "chance to win", sep = " ")
+
+
 games
 
-gamedate <- "2017-10-04twitter.csv"
+
+tweetdate <- as.Date("2017-10-04", "%Y-%m-%d")
+
+gamedate <- paste(tweetdate, "twitter.csv", sep = "")
+
+# Macbook path save
 write.csv(games, file = paste("/Users/derekpeterson/Documents/R/Elo/Twitter/", gamedate, sep = ""))
+
+# PC path save
+write.csv(games, file = paste("~/R/Elo/Elo2/Twitter/", gamedate, sep = ""))
+
+
+
+
+
+
+
+
+
 
