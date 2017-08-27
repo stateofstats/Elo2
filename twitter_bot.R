a2f97d6703df773d89fa1a1132e4cb38477c2660
stateofstats <depepeterson23@gmail.com>
2017-08-27 14:11:55 -0500

    dual paths/tweet delay update

--- twitter_bot.R
+++ twitter_bot.R
@@ -2,9 +2,15 @@
 
 require(twitteR)
 
+# Macbook set path
 setwd("/Users/derekpeterson/Documents/R/Elo/Twitter")
 
-api_keys <- read.csv(file = "credentials.csv", stringsAsFactors = FALSE)
+# PC set path
+setwd("~/R/Elo/Elo2/Twitter/")
+
+
+
+api_keys <- read.csv(file = "credentials.csv", stringsAsFactors = FALSE, header = TRUE)
 
 setup_twitter_oauth(consumer_key = api_keys$consumer_key,
                     consumer_secret = api_keys$consumer_secret,
@@ -13,9 +19,22 @@ setup_twitter_oauth(consumer_key = api_keys$consumer_key,
 
 tweetday <- "2017-10-04"
 games <- read.csv(file = paste(tweetday,"twitter.csv", sep = ""))
-
 games$Tweet <- as.character(games$Tweet)
 
 
+# set time for tweets to be sent in 24hr format
 
-lapply(tweet_list, tweet)
+tweet_list <- games$Tweet
+tweetdelay <- function(x, sendtime) {
+  sendtime <- as.POSIXct(paste(Sys.Date(), sendtime, sep = " "), format = "%Y-%m-%d %H:%M:%OS")
+  Sys.sleep(sendtime - Sys.time())
+  for (i in 1:length(x)) {
+    tweet(x[[i]])
+    Sys.sleep(10)
+  }
+}
+
+
+
+tweetdelay(tweet_list, "14:08:30")
+
